f9ef6918f0652e5c3435b36c5ce7bd6f7a6723ea
Sloven <vladimir@ruban.pro>
2017-02-14 14:07:42 +0200

    TOKEN for a year for testing purposes

--- test/fakes/CONSTfake.js
+++ test/fakes/CONSTfake.js
@@ -11,5 +11,8 @@
     api_auth: 'api/authenticate',
     api_token: 'http://localhost/linkManager/oauth/token',
 
-    demoUserPrefix: 'userForDemo#'
+    demoUserPrefix: 'userForDemo#',
+
+    TestAccessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTM4NCJ9.eyJuYW1laWQiOiI4OWJlNjEzYS1iYmUzLTQ0OTYtOTQ0Ny03N2JjYTkzMzA4ZGYiLCJ1bmlxdWVfbmFtZSI6IlRlc3QiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL2FjY2Vzc2NvbnRyb2xzZXJ2aWNlLzIwMTAvMDcvY2xhaW1zL2lkZW50aXR5cHJvdmlkZXIiOiJBU1AuTkVUIElkZW50aXR5IiwiQXNwTmV0LklkZW50aXR5LlNlY3VyaXR5U3RhbXAiOiJiM2I4YWU5NS1lYWJlLTQyNGMtOWQ2Yy03ZDUxNDEwMThkNTIiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L0xpbmtNYW5hZ2VyIiwiYXVkIjoiMCIsImV4cCI6MTUxODYwOTM4OSwibmJmIjoxNDg3MDczMzg5fQ.mYJBmuoCF0L-_NcX0p9dWntXtZhu3eh1nC5w0BUuxIafIlNy7Ke11HfaVVZffj80'
+
 };
